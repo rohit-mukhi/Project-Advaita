65ceb791b8697ac582559a24b7dc22ca651d84ac
Rohit Mukhi <rohitmukhi52@gmail.com>
2026-03-02 21:19:34 +0530

    Added loading page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from './Landing'
+import LoadingScreen from './LoadingScreen'
 import Home from './Home'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<LoadingScreen />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
---
+++ src/LoadingScreen.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import Landing from './Landing'
+import logo from './assets/Loading-Screen/logo.png'
+import './LoadingScreen.css'
+
+function LoadingScreen() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (loading) {
+    return (
+      <div className="loading-screen">
+        <img src={logo} alt="Aroha Logo" className="loading-logo" />
+        <h1 className="loading-title">Aroha</h1>
+      </div>
+    )
+  }
+
+  return <Landing />
+}
+
+export default LoadingScreen
